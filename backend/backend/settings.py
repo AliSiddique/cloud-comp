0ebf4b1d34ff073be2438fa57247827b95d8a674
AliSiddique <alisiddique10@hotmail.com>
2023-07-13 21:37:04 +0100

    S

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -171,7 +171,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:8081","http://127.0.0.1:3000","http://127.0.0.1:8081",'https://next-django-saas-template.vercel.app']
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:8081","http://127.0.0.1:3000","http://127.0.0.1:8081",'https://next-django-saas-template.vercel.app','https://next-django-saas-template-production.up.railway.app']
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
