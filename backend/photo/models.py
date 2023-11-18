04784f5b3011cbece421f7ef1700f9bbabbbc014
AliSiddique <alisiddique10@hotmail.com>
2023-11-18 15:04:56 +0000

    AA

--- backend/photo/models.py
+++ backend/photo/models.py
@@ -3,8 +3,6 @@ from django.db import models
 # Create your models here.
 class Photo(models.Model):
     image = models.ImageField(upload_to='images/')
-    file_size = models.CharField(max_length=200, default="0", blank=True)
-    file_type = models.CharField(max_length=200, default="0", blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     
     user = models.ForeignKey('auth.User', related_name='photos', on_delete=models.CASCADE)
