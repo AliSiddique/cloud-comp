3d294eebf6a90026152eae263076e42f875969d7
AliSiddique <alisiddique10@hotmail.com>
2023-12-30 19:07:14 +0000

    AS

--- frontend/src/components/Layout/Dashboard/DashboardLayout.tsx
+++ frontend/src/components/Layout/Dashboard/DashboardLayout.tsx
@@ -26,13 +26,11 @@ const navigation = [
   { name: 'Photos', href: '#', icon: PhotoIcon, current: true },
 ]
 const userNavigation = [
-  { name: 'Your profile', href: '#' },
   { name: 'Sign out', href: '#' },
 ]
 const tabs = [
   { name: 'Recently Viewed', href: '#', current: true },
-  { name: 'Recently Added', href: '#', current: false },
-  { name: 'Favorited', href: '#', current: false },
+
 ]
 
 const currentFile = {
@@ -47,20 +45,7 @@ const currentFile = {
     Dimensions: '4032 x 3024',
     Resolution: '72 x 72',
   },
-  sharedWith: [
-    {
-      id: 1,
-      name: 'Aimee Douglas',
-      imageUrl:
-        'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=1024&h=1024&q=80',
-    },
-    {
-      id: 2,
-      name: 'Andrea McMillan',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=oilqXxSqey&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  ],
+
 }
 
 function classNames(...classes: string[]) {
@@ -455,36 +440,7 @@ export default function DashboardLayout({token,children}:any) {
                   </div>
                 </div>
                 <div>
-                  <h3 className="font-medium text-gray-900">Shared with</h3>
-                  <ul role="list" className="mt-2 divide-y divide-gray-200 border-b border-t border-gray-200">
-                    {currentFile.sharedWith.map((person) => (
-                      <li key={person.id} className="flex items-center justify-between py-3">
-                        <div className="flex items-center">
-                          <img src={person.imageUrl} alt="" className="h-8 w-8 rounded-full" />
-                          <p className="ml-4 text-sm font-medium text-gray-900">{person.name}</p>
-                        </div>
-                        <button
-                          type="button"
-                          className="ml-6 rounded-md bg-white text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        >
-                          Remove<span className="sr-only"> {person.name}</span>
-                        </button>
-                      </li>
-                    ))}
-                    <li className="flex items-center justify-between py-2">
-                      <button
-                        type="button"
-                        className="group -ml-1 flex items-center rounded-md bg-white p-1 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                      >
-                        <span className="flex h-8 w-8 items-center justify-center rounded-full border-2 border-dashed border-gray-300 text-gray-400">
-                          <PlusIcon className="h-5 w-5" aria-hidden="true" />
-                        </span>
-                        <span className="ml-4 text-sm font-medium text-indigo-600 group-hover:text-indigo-500">
-                          Share
-                        </span>
-                      </button>
-                    </li>
-                  </ul>
+
                 </div>
                 <div className="flex gap-x-3">
                   <button
