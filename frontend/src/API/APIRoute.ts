1eeda70862a177da5ec885748b58b9d58c6c8148
AliSiddique <alisiddique10@hotmail.com>
2024-01-02 18:14:38 +0000

    SD

--- frontend/src/API/APIRoute.ts
+++ frontend/src/API/APIRoute.ts
@@ -1,7 +1,7 @@
 export let BASEURL:string;
 
 if(process.env.NODE_ENV === "production") {
-    BASEURL = "http://127.0.0.1:8000"
+    BASEURL = "http://0.0.0.0:8000"
 } else {
     BASEURL = "http://127.0.0.1:8000"
 }
